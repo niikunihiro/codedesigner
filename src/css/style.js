4d8afc08b1548c4079753d8871d878f70a65f0ce
niidashi <rohinikusidaini@me.com>
2016-09-20 21:21:35 +0900

    load font-awesome by webpack

--- src/css/style.js
+++ src/css/style.js
@@ -1,3 +1,4 @@
 'use strict';
 
 require('bulma');
+require('font-awesome/css/font-awesome.css');
--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,21 @@ module.exports = {
       {
         test: /\.s(a|c)ss$/,
         loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')
+      }, {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/font-woff"
+      }, {
+        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/font-woff"
+      }, {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/octet-stream"
+      }, {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file"
+      }, {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=image/svg+xml"
       }
     ]
   },
